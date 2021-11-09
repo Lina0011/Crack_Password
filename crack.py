52bb90710e425ddd7c3bcc5cc31cefb821a7b41f
Lina <79867485+Lina0011@users.noreply.github.com>
2021-11-09 21:27:30 +0300

    Add files via upload

---
+++ crack.py
@@ -0,0 +1,34 @@
+import hashlib
+
+flag = 0
+
+pass_hash = input("Enter hash: ")
+
+wordlist = input("File name: ")
+
+try:
+	pass_file = open (wordlist, "r")
+except:
+	print("No file found")
+	quit()
+
+for word in pass_file:
+
+	enc_wrd = word.encode('utf-8')
+	digest = hashlib.md5(enc_wrd.strip()).hexdigest()
+
+	#print(word)
+	#print(digest)
+	#print(pass_hash)
+
+	if digest == pass_hash:
+		print("password found")
+		print(word)
+		flag = 1
+		break
+
+if flag == 0:
+	print("not found")
+
+
+
